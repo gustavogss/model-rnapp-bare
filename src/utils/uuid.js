a3ef8278a4a6dcf7fea40e28f122712262787a62
gustavogss <gustavogss.dev@gmail.com>
2023-07-19 06:23:00 -0300

    Criando ids únicos

---
+++ src/utils/uuid.js
@@ -0,0 +1,15 @@
+// Define ids únicos
+
+export function uuid(){
+    let dateTime = new Date().getTime();
+    let uuid = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
+      /[xy]/g,
+      (i) => {
+        let random = (dateTime + Math.random() * 16) % 16 | 0;
+        dateTime = Math.floor(dateTime / 16);
+        return (i == "x" ? random : (random & 0x3) | 0x8).toString(16);
+      }
+    );
+    return uuid;
+  }
+  
